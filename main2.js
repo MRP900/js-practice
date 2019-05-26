e662a42a1c0f9192928385c299caeb72b7b90cd2
Matt <mattrpittman@gmail.com>
2019-05-25 20:08:03 -0600

    mains2.js

--- main2.js
+++ main2.js
@@ -3,7 +3,19 @@
 // Single element
 // console.log(document.getElementById('my-form'));
 // console.log(document.querySelector('.container'));
-
+// console.log(document.getElementsByClassName('.item'));
 // Multiple element
-// console.log(document.querySelectorAll('.item'));
-console.log(document.getElementsByClassName('.item'));
+// const items = document.querySelectorAll('.item');
+
+// items.forEach((item) => console.log(item));
+
+const ul = document.querySelector('.items');
+
+// ul.remove();
+// ul.lastElementChild.remove();
+ul.firstElementChild.textContent = 'hello';
+ul.children[1].innerText = 'brad';
+ul.lastElementChild.innerHTML = '<h1>Hello</h1>';
+
+const btn = document.querySelector('.btn');
+btn.style.background = 'red';
